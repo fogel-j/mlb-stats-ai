849d91d0d50f8e0b7ce96e1b23242637a37bf4ab
Joe Fogelman <joseph.fogelman@solvitursystems.com>
2023-05-25 17:02:46 -0400

    initial openai sports template

--- main.py
+++ main.py
@@ -1,56 +1,138 @@
 import json
+import requests
+import urllib.parse
 
 import quart
 import quart_cors
 from quart import request
 
+import pybaseball
+
+# Note: Setting CORS to allow chat.openapi.com is only required when running a localhost plugin
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
+HOST_URL = "http://localhost:5003"
+ESPN_URL = "https://www.espn.com"
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
+@app.get("/standings")
+async def get_standings():
+    # Return the overall MLB standings
+    return "Success", 200
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
 
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+@app.get("/players")
+async def get_players():
+    query = request.args.get("query")
+    res = requests.get(
+        f"{ESPN_URL}/search/_/q/{query}")
+    body = res.json()
+    return quart.Response(response=json.dumps(body), status=200)
+
+
+@app.get("/teams")
+async def get_teams():
+    res = requests.get(
+        "{HOST_URL}/api/v1/teams?page=0&per_page=100")
+    body = res.json()
+    return quart.Response(response=json.dumps(body), status=200)
+
+
+@app.get("/games")
+async def get_games():
+    query_params = [("page", "0")]
+    limit = request.args.get("limit")
+    query_params.append(("per_page", limit or "100"))
+    start_date = request.args.get("start_date")
+    if start_date:
+        query_params.append(("start_date", start_date))
+    end_date = request.args.get("end_date")
+
+    if end_date:
+        query_params.append(("end_date", end_date))
+    seasons = request.args.getlist("seasons")
+
+    for season in seasons:
+        query_params.append(("seasons[]", str(season)))
+    team_ids = request.args.getlist("team_ids")
+
+    for team_id in team_ids:
+        query_params.append(("team_ids[]", str(team_id)))
+
+    res = requests.get(
+        f"{HOST_URL}/api/v1/games?{urllib.parse.urlencode(query_params)}")
+    body = res.json()
+    return quart.Response(response=json.dumps(body), status=200)
+
+
+@app.get("/stats")
+async def get_stats():
+    query_params = [("page", "0")]
+    limit = request.args.get("limit")
+    query_params.append(("per_page", limit or "100"))
+    start_date = request.args.get("start_date")
+    if start_date:
+        query_params.append(("start_date", start_date))
+    end_date = request.args.get("end_date")
+
+    if end_date:
+        query_params.append(("end_date", end_date))
+    player_ids = request.args.getlist("player_ids")
+
+    for player_id in player_ids:
+        query_params.append(("player_ids[]", str(player_id)))
+    game_ids = request.args.getlist("game_ids")
+
+    for game_id in game_ids:
+        query_params.append(("game_ids[]", str(game_id)))
+    res = requests.get(
+        f"{HOST_URL}/api/v1/stats?{urllib.parse.urlencode(query_params)}")
+    body = res.json()
+    return quart.Response(response=json.dumps(body), status=200)
+
+
+@app.get("/season_averages")
+async def get_season_averages():
+    query_params = []
+    season = request.args.get("season")
+    if season:
+        query_params.append(("season", str(season)))
+    player_ids = request.args.getlist("player_ids")
+
+    for player_id in player_ids:
+        query_params.append(("player_ids[]", str(player_id)))
+    res = requests.get(
+        f"{HOST_URL}/api/v1/season_averages?{urllib.parse.urlencode(query_params)}")
+    body = res.json()
+    return quart.Response(response=json.dumps(body), status=200)
+
 
 @app.get("/logo.png")
 async def plugin_logo():
     filename = 'logo.png'
     return await quart.send_file(filename, mimetype='image/png')
 
+
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
     host = request.headers['Host']
-    with open("./.well-known/ai-plugin.json") as f:
+    with open("ai-plugin.json") as f:
         text = f.read()
+        # This is a trick we do to populate the PLUGIN_HOSTNAME constant in the manifest
+        text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
         return quart.Response(text, mimetype="text/json")
+
 
 @app.get("/openapi.yaml")
 async def openapi_spec():
     host = request.headers['Host']
     with open("openapi.yaml") as f:
         text = f.read()
+        # This is a trick we do to populate the PLUGIN_HOSTNAME constant in the OpenAPI spec
+        text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
         return quart.Response(text, mimetype="text/yaml")
-
-def main():
-    app.run(debug=True, host="0.0.0.0", port=5003)
+@app.errorhandler(404)
+async def page_not_found(e):
+    return "Page Not Found" , 404
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True, host='0.0.0.0', port=5003)
